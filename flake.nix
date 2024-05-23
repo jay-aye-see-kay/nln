96364e4645fd72a988e606203b7369f81ac668d1
Jack Rose <user@jackrose.co.nz>
2024-05-23 22:52:17 +1000

    actually kind of working

--- flake.nix
+++ flake.nix
@@ -17,8 +17,6 @@
 
         utils = (import ./utils.nix) { inherit pkgs; };
 
-        # depsTable = fpkgs.writeText "included.lua" ''return ${(import ../utils).luaTablePrinter allPluginDeps}'';
-
         makeNeovim =
           { extraPkgsPath ? ""
           , extraPython3Packages ? (p: [ ])
@@ -30,8 +28,10 @@
           }:
           let
             luaPath = "${./.}";
-            LuaConfig = pkgs.stdenv.mkDerivation {
-              name = "nixCats-special-rtp-entry-LuaConfig";
+
+            # user config as a plugin
+            userConfig = pkgs.stdenv.mkDerivation {
+              name = "nln-user-config";
               builder = pkgs.writeText "builder.sh" /* bash */ ''
                 source $stdenv/setup
                 mkdir -p $out
@@ -39,20 +39,64 @@
               '';
             };
 
+            # convert a list of plugins into a dict we can modify, then pass to lazy.nvim
+            pluginsForConfig = builtins.foldl'
+              (acc: elem: { "${elem.pname}" = { }; } // acc)
+              { }
+              plugins;
+            pluginDirs = builtins.foldl'
+              (acc: elem: { "${elem.pname}" = "${elem}"; } // acc)
+              { }
+              plugins;
+
+            # this file is how we pass build info (like paths) to lua config
+            generatedLuaFile = pkgs.writeText "generated.lua" /* lua */ ''
+              local M = {}
+              -- keep private so can't be modified, map pnames to store path
+              pluginDirs = ${utils.luaTablePrinter pluginDirs}
+              -- mutable list of plugins to collect config
+              M.plugins = ${utils.luaTablePrinter pluginsForConfig}
+              -- method to call after configuring to convert to list for lazy.nvim
+              function M.plugins:for_lazy()
+                local result = {}
+                for p_name, p_cfg in pairs(self) do
+                  if type(p_cfg) ~= "function" then
+                    local lazy_spec = vim.tbl_extend("force", p_cfg, { dir = pluginDirs[p_name] })
+                    table.insert(result, lazy_spec)
+                  end
+                end
+                return result
+              end
+              return M
+            '';
+
+            # plugin containing paths etc generated at build time
+            nlnPlugin = pkgs.stdenv.mkDerivation {
+              name = "nln-plugin";
+              builder = pkgs.writeText "builder" /* bash */ ''
+                source $stdenv/setup
+                mkdir -p $out/lua/nln
+                cat ${generatedLuaFile} > $out/lua/nln/init.lua
+              '';
+            };
+
             cfg = pkgs.neovimUtils.makeNeovimConfig {
-              inherit
-                extraPkgsPath extraPython3Packages plugins withNodeJs withPython3;
+              inherit extraPkgsPath extraPython3Packages withNodeJs withPython3;
+              plugins = plugins ++ [ pkgs.vimPlugins.lazy-nvim ];
               customRC = /* vim */ ''
                 lua << EOF
-                  vim.opt.rtp:prepend("${LuaConfig}")
-                EOF
+                  -- Ignore the user lua configuration
+                  vim.opt.runtimepath:remove(vim.fn.stdpath("config")) -- ~/.config/nvim
+                  vim.opt.runtimepath:remove(vim.fn.stdpath("config") .. "/after") -- ~/.config/nvim/after
+                  vim.opt.runtimepath:remove(vim.fn.stdpath("data") .. "/site") -- ~/.local/share/nvim/site
 
-                let configdir = "${LuaConfig}"
-                if filereadable(configdir . "/init.lua")
-                  execute "source " . configdir . "/init.lua"
-                elseif filereadable(configdir . "/init.vim")
-                  execute "source " . configdir . "/init.vim"
-                endif
+                  vim.opt.rtp:prepend("${userConfig}")
+                  vim.opt.rtp:prepend("${nlnPlugin}")
+
+                  if vim.fn.filereadable("${userConfig}/init.lua") then
+                    vim.cmd("source ${userConfig}/init.lua")
+                  end
+                EOF
               '';
             };
           in
@@ -63,6 +107,7 @@
         mainNeovim = makeNeovim {
           plugins = with pkgs.vimPlugins; [
             vim-fugitive
+            zoxide-vim
           ];
           extraPkgsPath = pkgs.lib.makeBinPath (with pkgs; [
             # nil # nix lsp
--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 print("hi from root init.lua")
 require("_cfg.treesitter")
+
+local plugins = require("nln").plugins
+vim.print(plugins)
+
+plugins["vim-fugitive"] = {
+	lazy = true,
+	cmd = "G",
+}
+
+require("lazy").setup(plugins:for_lazy())
